04730e4eacacc11a9fea17e42001a48b7e9b92e9
Dzaki-Akram <h1101231056@student.untan.ac.id>
2025-05-25 14:53:06 +0700

    Instalasi Sanctum dan Spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens,Notifiable,HasRoles;
+
 
     protected $fillable = [
         'name',
