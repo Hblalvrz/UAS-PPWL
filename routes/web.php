b52566de16d4ea0fbda30b18f24a653e446459bd
Habiel Alvarezi <H1101231016@student.untan.ac.id>
2025-06-01 09:50:13 +0700

    Penyesuaian Routes

--- routes/web.php
+++ routes/web.php
@@ -5,33 +5,26 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\LaundryProviderController;
 use App\Http\Controllers\API\LaundryServiceController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\API\ReviewController;
 use App\Http\Controllers\API\UserController;
 use App\Models\LaundryService;
 
-Route::get('/', function () {
+Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/', [AuthController::class, 'login']);
 
-    return view('login.auth.login')->name('login');
-});
+Route::get('/laundry/dashboard', function () {
+    return view('laundry.dashboard.index');
+})->middleware('auth')->name('laundry.dashboard.index');
 
-Route::get('/laundry/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('laundry.dashboard.index');
-
-// Route::get('/laundry/dashboard', function () {
-//     return view('laundry.dashboard.index');
-// })->middleware('auth')->name('laundry.dashboard.index');
-
-Route::get('/customer/dashboard', function () {
-    return view('customer.dashboard.index');
-})->middleware('auth')->name('customer.dashboard.index');
+Route::get('/customer/dashboard', [LaundryProviderController::class, 'searching'])->name('customer.dashboard.index');
 
 Route::get('/provider', function () {
     return view('customer.cari.cari');
 })->middleware('auth')->name('provider.index');
 
-Route::get('/order-provider', function () {
-    return view('customer.cari.order');
-})->middleware('auth')->name('customer.order');
+Route::get('/customer/order/{provider}', [OrderController::class, 'create'])->name('customer.order');
+
+Route::post('/customer/order', [OrderController::class, 'storecustomer'])->name('customer.order.store');
 
 Route::get('/riwayat-customer', function () {
     return view('customer.riwayat.riwayat');
@@ -45,6 +38,6 @@ Route::resource('orders', OrderController::class);
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+
+
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
