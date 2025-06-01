2519309e683628bd700b9e112f3814731c6221d9
Habiel Alvarezi <H1101231016@student.untan.ac.id>
2025-06-01 10:02:05 +0700

    Perbaikan Routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OrderController;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\LaundryProviderController;
 use App\Http\Controllers\API\LaundryServiceController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\API\ReviewController;
 use App\Http\Controllers\API\UserController;
 use App\Models\LaundryService;
@@ -12,19 +13,23 @@ use App\Models\LaundryService;
 Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/', [AuthController::class, 'login']);
 
-Route::get('/laundry/dashboard', function () {
-    return view('laundry.dashboard.index');
-})->middleware('auth')->name('laundry.dashboard.index');
+Route::get('/laundry/dashboard', [DashboardController::class, 'index'])->middleware('auth')->name('laundry.dashboard.index');
 
-Route::get('/customer/dashboard', [LaundryProviderController::class, 'searching'])->name('customer.dashboard.index');
+// Route::get('/laundry/dashboard', function () {
+//     return view('laundry.dashboard.index');
+// })->middleware('auth')->name('laundry.dashboard.index');
+
+Route::get('/customer/dashboard', function () {
+    return view('customer.dashboard.index');
+})->middleware('auth')->name('customer.dashboard.index');
 
 Route::get('/provider', function () {
     return view('customer.cari.cari');
 })->middleware('auth')->name('provider.index');
 
-Route::get('/customer/order/{provider}', [OrderController::class, 'create'])->name('customer.order');
-
-Route::post('/customer/order', [OrderController::class, 'storecustomer'])->name('customer.order.store');
+Route::get('/order-provider', function () {
+    return view('customer.cari.order');
+})->middleware('auth')->name('customer.order');
 
 Route::get('/riwayat-customer', function () {
     return view('customer.riwayat.riwayat');
@@ -38,6 +43,4 @@ Route::resource('orders', OrderController::class);
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
-
-
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
